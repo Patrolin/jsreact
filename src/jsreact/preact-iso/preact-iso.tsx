7e856e2db825cb66b5aff19e29b10973a2c25f9a
Patrolin <patrik@balas.cz>
2026-02-22 18:40:42 +0100

    preact Router component

---
+++ src/jsreact/preact-iso/preact-iso.tsx
@@ -0,0 +1,98 @@
+import { createContext, FC, Fragment, PropsWithChildren, useContext, useRef } from "../jsreact";
+
+// LocationProvider
+function route(url: string, replace?: boolean): void {
+  throw new Error("route() not yet implemented in jsreact!");
+}
+type LocationContextType = {
+  pathname: string;
+  url: string;
+  query: Record<string, string>;
+  route: typeof route;
+};
+const LocationContext = createContext<LocationContextType | undefined>(undefined);
+type LocationProviderProps = {};
+export const LocationProvider: FC<LocationProviderProps> = (props) => {
+  const locationRef = useRef<LocationContextType>({
+    pathname: "",
+    url: "",
+    query: {},
+    route,
+  });
+  const path = window.location.pathname;
+  locationRef.current.pathname = path;
+  const search = window.location.search;
+  const newUrl = path + search;
+  if (newUrl !== locationRef.current.url) {
+    const query = {} as Record<string, string>;
+    for (let [k, v] of new URLSearchParams(search).entries()) {
+      query[k] = v;
+    }
+    locationRef.current.query = query;
+  }
+  locationRef.current.url = newUrl;
+  return <LocationContext value={locationRef.current} children={props.children} />;
+};
+export function useLocation(): LocationContextType {
+  return useContext(LocationContext)!;
+}
+
+// router
+type RouteProps = { path?: string; default?: boolean; component: React.JSXElementConstructor<any> };
+export const Route = Fragment as FC<RouteProps>;
+
+type RouterContextType = {
+  path: string;
+  query: Record<string, string>;
+  params: Record<string, string>;
+};
+const RouterContext = createContext<RouterContextType | undefined>(undefined);
+type RouterProps = {};
+export const Router: FC<PropsWithChildren<RouterProps>> = (props) => {
+  const routerRef = useRef<RouterContextType>({
+    path: "",
+    query: {},
+    params: {},
+  });
+  // find matching route
+  const { children } = props;
+  if (!Array.isArray(children)) return;
+  const { pathname, query } = useLocation();
+  let newParams = {} as Record<string, string>;
+  let selectedRoute: RouteProps|undefined;
+  let defaultRoute: RouteProps|undefined;
+  for (let child of children) {
+    const route = child.props as RouteProps;
+    const { path, default: isDefault } = route;
+    newParams = {};
+    if (isDefault || path === "*") {
+      defaultRoute = route;
+    } else if (path) {
+      let patternOffset = 0;
+      let matchOffset = 0;
+      let isMatchingPath = true;
+      while (patternOffset < path.length || matchOffset < pathname.length) {
+        const pattern = path.slice(patternOffset).match(/:([^:/]*)/);
+        const stringEnd = pattern != null ? pattern.index! : Infinity;
+        isMatchingPath &&= path.slice(patternOffset, patternOffset+stringEnd) === pathname.slice(matchOffset, matchOffset+stringEnd);
+        if (pattern == null) break;
+        const match = pathname.slice(matchOffset).slice(pattern.index).match(/[^/]*/)!;
+        newParams[pattern[1]] = match[0];
+        patternOffset += pattern.index! + pattern[0].length;
+        matchOffset += pattern.index! + match[0].length;
+      }
+      if (isMatchingPath) {
+        selectedRoute = route;
+      }
+    }
+  }
+  if (selectedRoute == null) selectedRoute = defaultRoute;
+  // set RouterContext
+  routerRef.current.path = pathname;
+  routerRef.current.query = query;
+  routerRef.current.params = {}; // TODO: BIN-26 route params
+  const SelectedComponent = selectedRoute?.component;
+  return <RouterContext value={routerRef.current}>
+    {SelectedComponent && <SelectedComponent />}
+  </RouterContext>;
+};
